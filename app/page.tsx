6ba724d3e1059d7a94686d2182405c6a4175ae5e
Kiru100 <79371033+Kiru100@users.noreply.github.com>
2024-12-18 21:49:27 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,15 +35,15 @@ export default function Home() {
 				</div>
 				
 			</div>
-			<div className="container mx-auto px-4">
-				<div className="grid grid-cols-2 place-items-center h-full">
-				<div className="text-5xl text-white z-10">
-					<h1 className="text-5xl font-bold sm:text-6xl">$NewYears</h1>
-					<p className="text-lg sm:text-xl max-w-3xl mx-auto">
-					Celebrate, meme, and moon your way into 2025 with $NewYears.  
-					A coin built for laughs, gains, and an all-year party vibe. 🎉✨
-				</p>
-				</div>
+			<div className="container mx-auto pt-20 ">
+				<div className="grid grid-cols-2">
+					<div className="flex flex-col ">
+						<h1 className="text-4xl md:text-7xl font-bold mb-6 relative text-left dark:text-zinc-100 text-zinc-700 max-w-4xl">$NewYears</h1>
+						<p className="relative text-sm sm:text-xl text-zinc-500 dark:text-zinc-300 tracking-wide mb-8 text-left max-w-2xl antialiased leading-loose">
+							Celebrate, meme, and moon your way into 2025 with $NewYears.  
+							A coin built for laughs, gains, and an all-year party vibe. 🎉✨
+						</p>
+					</div>
 				<div className="text-5xl text-white z-10">Happy New Year 2025</div>
 				</div>
 			</div>
