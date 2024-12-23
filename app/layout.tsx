bbc1ed51348d3edb62a71192804012b8f32b8ebf
Kiru100 <79371033+Kiru100@users.noreply.github.com>
2024-12-23 08:04:01 +0800

    added font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Inter, Roboto} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -7,10 +7,9 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"], variable: "--font_inter" });
+const roboto = Roboto({ subsets: ["latin"], variable: "--font_roboto", weight: ["100","300", "400", "500", "700"] });
+
 
 export const metadata: Metadata = {
   title: "$NewYears",
@@ -25,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${roboto.variable} $${inter.variable} antialiased`}
       >
         {children}
       </body>
