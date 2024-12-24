31a6415481dc5cf56b2f8ff4ac0f3aa074892cba
Kiru100 <79371033+Kiru100@users.noreply.github.com>
2024-12-25 03:18:19 +0800

    updated footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,25 +7,28 @@ import { FlipWords } from './ui/flip-words'
 export default function Footer() {
     return (
         <div className="relative z-20 py-10 max-w-7xl mx-auto">
-            <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center md:justify-between">
-                <div className="flex-row flex items-center gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 md:justify-between px-8 gap-8">
+                <div className="flex flex-col-reverse md:flex-row justify-center md:justify-start items-center gap-3">
                     <Link href="/" className="flex flex-row items-center gap-2">
                         <Image
                             className="rounded-full self-center"
-                            width={64}
-                            height={64}
+                            width={72}
+                            height={72}
                             src={`https://dd.dexscreener.com/ds-data/tokens/solana/E8F3PaD1StLs1HYqWeutGji15DhNTJ6yp2LGWVNNFiuH.png?size=xl&key=2c39fb`}
                             alt="Twenty Twenty Five"
                         />	
-                    </Link>     
-                    <h3 className="text-slate-300 font-inter-medium text-xl">
+                    </Link>  
+                    <h3 className="text-start max-w-5xl tracking-tight text-black dark:text-white font-inter-medium text-lg md:leading-snug">
                         New{<FlipWords words={["Year", "Me"]} />}
-                    </h3>         
+                    </h3>             
                 </div>
-                <div className="justify-end flex">
-                    <SocialLink />
+                <div className="flex justify-center md:justify-end">
+                    <div className="flex flex-col gap-4 justify-center md:justify-start">
+                        <h3 className="text-center md:text-start max-w-5xl tracking-tight text-black dark:text-white font-inter-medium text-base md:leading-snug">Find us on</h3>
+                        <SocialLink />
+                    </div>
                 </div>
             </div>
         </div>
     )
-}
+};
