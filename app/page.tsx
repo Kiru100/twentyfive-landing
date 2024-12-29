654c6d933dced5a502c1f1a3df3bb7af3c19f48b
Kiru100 <79371033+Kiru100@users.noreply.github.com>
2024-12-30 02:28:55 +0800

    added 500x boosted and show happy new once countdown is done

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,10 @@ import { FeaturesSection } from "@/components/FeaturesSection";
 import { useEffect } from "react";
 import TokeDetails from "@/components/TokenDetails";
 import Footer from "@/components/Footer";
+import Marquee from "react-fast-marquee";
+import Image from "next/image";
+import Boosted_Image from "@/public/booster.png";
+import Link from "next/link";
 
 export default function Home() {
 
@@ -16,7 +20,25 @@ export default function Home() {
 
 	return (
 		<>	
-			<div className="h-screen p-8 flex flex-col overflow-hidden max-w-7xl mx-auto">
+			<Marquee 
+				autoFill 
+				speed={50} 
+				direction="left" 
+				className="bg-yellow-950 absolute"
+			>	
+			<Link 
+				href="https://dexscreener.com/solana/e8f3pad1stls1hyqweutgji15dhntj6yp2lgwvnnfiuh" 
+				target="_blank" 
+			>
+				<Image 	
+					width={100}
+					height={100}				
+					className="h-full w-full object-cover"
+					src={"https://media.discordapp.net/attachments/1321143163074646016/1322987158805418054/Screenshot_2024-12-30_015254.png?ex=6772df4e&is=67718dce&hm=b8953240bf75c9cd752e84103d8948b609f0db993971d39dbd1598a18d483e9b&=&format=webp&quality=lossless"} alt="Moving picture of Leonardo DiCaprio" 
+				/>
+			</Link>
+			</Marquee>
+			<div className="h-screen p-8 flex flex-col overflow-hidden max-w-7xl mx-auto">	
 				<TokenLogo />
 				<Hero/>
 				<ParticleBG />
