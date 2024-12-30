3cf69834390f594d8298b4c6698a3c749e74505c
Kiru100 <79371033+Kiru100@users.noreply.github.com>
2024-12-31 05:23:48 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,7 @@ import ParticleBG from "@/components/ParticleBG";
 import TokeDetails from "@/components/TokenDetails";
 import TokenLogo from "@/components/TokenLogo";
 import NewYearCountdown from "@/components/ui/new-year-countdown";
-import Image from "next/image";
-import Link from "next/link";
 import { useEffect } from "react";
-import Marquee from "react-fast-marquee";
 
 export default function Home() {
 
@@ -19,24 +16,6 @@ export default function Home() {
 
 	return (
 		<>	
-			<Marquee 
-				autoFill 
-				speed={50} 
-				direction="left" 
-				className="bg-yellow-950 absolute"
-			>	
-				<Link 
-					href="https://dexscreener.com/solana/e8f3pad1stls1hyqweutgji15dhntj6yp2lgwvnnfiuh" 
-					target="_blank" 
-				>
-					<Image 	
-						width={100}
-						height={100}				
-						className="h-full w-full object-cover"
-						src={"https://media.discordapp.net/attachments/1321143163074646016/1322987158805418054/Screenshot_2024-12-30_015254.png?ex=6772df4e&is=67718dce&hm=b8953240bf75c9cd752e84103d8948b609f0db993971d39dbd1598a18d483e9b&=&format=webp&quality=lossless"} alt="Moving picture of Leonardo DiCaprio" 
-					/>
-				</Link>
-			</Marquee>
 			<div className="h-screen p-8 flex flex-col overflow-hidden max-w-7xl mx-auto">	
 				<TokenLogo />
 				<Hero/>
